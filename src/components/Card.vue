bae9957706cfe462263acb96dd8e40478ee7a321
fwwieffering <fwieffering@gmail.com>
2019-07-05 08:12:48 -0500

    add rudimentary select highlighting

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,5 +1,5 @@
 <template>
-    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" height="360" width="235">
+    <svg style="cursor: pointer" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" height="360" width="235">
     <defs>
         <pattern id="hatch-green" width="5" height="3" patternUnits="userSpaceOnUse">
             <line class="green" x1="0" y1="0" x2="0" y2="10" stroke-width="2"></line>
@@ -14,6 +14,7 @@
         <polygon class="common" id="diamond" points="30,180 117,145 205,180 117,215"></polygon>
         <path class="common" id="squiggle" d="m 33,180 c 6,-13 11,-23 26,-28 15,-5 31,-1 58,7 23,7 36,0 43,-3 7,-4 17,-9 30,-9 13,0 18,21 12,33 -5,11 -14,23 -26,28 -12,5 -32,1 -59,-7 -17,-5 -29,-3 -42,3 -9,4 -18,10 -30,9 -13,0 -19,-18 -12,-33 z"></path>
     </defs>
+    <rect v-if="selected" x="5" y="5" height="350" width="225" rx="10" ry="10" fill="white" stroke="yellow" stroke-width="14"></rect>
     <rect x="5" y="5" height="350" width="225" rx="10" ry="10" fill="white" stroke="grey" stroke-width="2"></rect>
 
     <!-- single -->
@@ -60,6 +61,10 @@
 export default {
     name: 'Card',
     props: {
+        selected: {
+            type: Boolean,
+            default: false
+        },
         color: {
             type: String,
             required: true,
@@ -104,6 +109,10 @@ export default {
 
 
 <style>
+.highlight {
+    border-width: 2px;
+    border-color: yellow;
+}
 .common {
     stroke-width: 4;
     stroke-linejoin: round;
